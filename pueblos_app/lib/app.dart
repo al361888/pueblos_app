324ede8407eaa367c321f4d57667924917530c10
Miguel Ligero <al361888@uji.es>
2020-02-24 13:36:07 +0100

    Haciendo el login

--- pueblos_app/lib/app.dart
+++ pueblos_app/lib/app.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pueblos_app/authService.dart';
+import 'package:pueblos_app/screens/homeScreen.dart';
 import 'screens/loginScreen.dart';
 
 class App extends StatelessWidget {
+  AuthService appAuth = AuthService();
+
+  //Por defecto, la primera vez que inicias la app entras en el login
+  Widget _defaultHome = LoginScreen();
+  // Get result of the login function.
+  bool result = appAuth.login();
+
+  if (result) {
+    _defaultHome = new HomeScreen();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: _defaultHome,
     );
   }
 }
--- pueblos_app/lib/screens/loginScreen.dart
+++ pueblos_app/lib/screens/loginScreen.dart
@@ -1,23 +1,87 @@
-// TODO Implement this library.
 import 'package:flutter/material.dart';
+import 'package:pueblos_app/screens/homeScreen.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _logged = false;
+
+  _onPressed() {
+    HomeScreen();
   }
 
   @override
-  State<StatefulWidget> createState() => _LoginScreenState();
-    }
-  
-  class _LoginScreenState extends State<LoginScreen>{
-
-
-  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      body: _logged ? HomeScreen() : loginForm(),
+    );
+  }
+
+  Widget loginForm() {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Stack(fit: StackFit.expand, children: <Widget>[
+        new Image(
+          image: new AssetImage("assets/images/landscape.jpg"),
+          fit: BoxFit.cover,
+          colorBlendMode: BlendMode.darken,
+          color: Colors.black87,
+        ),
+        new Theme(
+          data: new ThemeData(
+              brightness: Brightness.dark,
+              inputDecorationTheme: new InputDecorationTheme(
+                // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
+                labelStyle:
+                    new TextStyle(color: Colors.tealAccent, fontSize: 25.0),
+              )),
+          isMaterialAppTheme: true,
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(),
+              new Container(
+                padding: const EdgeInsets.all(40.0),
+                child: new Form(
+                  autovalidate: true,
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                            labelText: "Enter Email", fillColor: Colors.white),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                      ),
+                      new MaterialButton(
+                        height: 50.0,
+                        minWidth: 150.0,
+                        color: Colors.green,
+                        splashColor: Colors.teal,
+                        textColor: Colors.white,
+                        child: new Icon(Icons.check),
+                        onPressed: _onPressed,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
