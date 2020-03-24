5416fc2d79edd916d501fbe6070cc80f527cbc9c
Mackyplz <miguelligeron@gmail.com>
2020-03-24 14:58:57 +0100

    Login screen done

--- pueblos_app/lib/screens/loginScreen.dart
+++ pueblos_app/lib/screens/loginScreen.dart
@@ -20,12 +20,12 @@ class _LoginScreenState extends State<LoginScreen> {
   _onPressed() async {
     AuthService authService = AuthService();
     print("Email: " + _userData.email);
-    await authService.login(_userData.email, _userData.pass).then((result){
+    await authService.login(_userData.email, _userData.pass).then((result) {
       _logged = result;
     });
-    if(_logged){
-      Navigator.push(context,
-              MaterialPageRoute(builder: (context) => HomeScreen()));
+    if (_logged) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
     }
   }
 
@@ -37,72 +37,128 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget loginForm() {
-    return new Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: new Stack(fit: StackFit.expand, children: <Widget>[
-        new Image(
-          image: new AssetImage("assets/images/landscape.jpg"),
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Image(
+          image: AssetImage("assets/images/landscape.jpg"),
           fit: BoxFit.cover,
-          colorBlendMode: BlendMode.darken,
-          color: Colors.black87,
         ),
-        new Theme(
-          data: new ThemeData(
-              brightness: Brightness.dark,
-              inputDecorationTheme: new InputDecorationTheme(
-                // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
-                labelStyle:
-                    new TextStyle(color: Colors.tealAccent, fontSize: 25.0),
-              )),
-          isMaterialAppTheme: true,
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new FlutterLogo(),
-              new Container(
-                padding: const EdgeInsets.all(40.0),
-                child: new Form(
-                  key: _formKey1,
-                  autovalidate: true,
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                            labelText: "Enter Email", fillColor: Colors.white),
-                        keyboardType: TextInputType.text,
-                        onChanged: (String value){
-                          this._userData.email = value;
-                        },
+        Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                stops: [0.1, 0.8],
+                colors: [Color(0xCC272741), Color(0x00272741)],
+              ),
+            )),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(
+                  left: 40, top: 20, right: 40, bottom: 20),
+              width: 350,
+              decoration: BoxDecoration(
+                color: Color(0x77090909),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.only(bottom: 20, top: 10),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Iniciar Sesión",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      )),
+                  Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.white70
+                    ),
+                    child: Form(
+                      key: _formKey1,
+                      autovalidate: true,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TextFormField(
+                            cursorColor: Theme.of(context).primaryColor,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                    color: Colors.white70,
+                                  )),
+                                icon: Icon(Icons.perm_identity),
+                                hintText: "Nombre de usuario",
+                                hintStyle: TextStyle(
+                                    color: Colors.white70, fontSize: 18)),
+                            keyboardType: TextInputType.text,
+                            onChanged: (String value) {
+                              this._userData.email = value;
+                            },
+                          ),
+                          TextFormField(
+                              cursorColor: Theme.of(context).primaryColor,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  fillColor: Colors.white,
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                    color: Colors.white70,
+                                  )),
+                                  icon: Icon(Icons.lock_outline),
+                                  hintText: "Contraseña",
+                                  hintStyle: TextStyle(
+                                      color: Colors.white70, fontSize: 18)),
+                              obscureText: true,
+                              keyboardType: TextInputType.text,
+                              onChanged: (String value) {
+                                this._userData.pass = value;
+                              }),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 60.0),
+                          ),
+                          Container(
+                            width: 380,
+                            height: 50,
+                            child: RaisedButton(
+                              color: Theme.of(context).primaryColor,
+                              onPressed: _onPressed,
+                              child: Container(
+                                  child: Text(
+                                "Acceder",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              )),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8.0))),
+                            ),
+                          )
+                        ],
                       ),
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "Enter Password",
-                        ),
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                        onChanged: (String value){
-                          this._userData.pass = value;
-                        }
-                      ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 60.0),
-                      ),
-                      new MaterialButton(
-                        height: 50.0,
-                        minWidth: 150.0,
-                        color: Colors.green,
-                        splashColor: Colors.teal,
-                        textColor: Colors.white,
-                        child: new Icon(Icons.arrow_forward),
-                        onPressed: _onPressed,
-                      )
-                    ],
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
+                ],
+              ),
+            ),
+            Container(
+                alignment: Alignment.bottomCenter,
+                child: FlatButton(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "¿No tienes cuenta? Regístrate",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  onPressed: () {},
+                )),
+          ],
         ),
       ]),
     );
--- pueblos_app/lib/screens/splashScreen.dart
+++ pueblos_app/lib/screens/splashScreen.dart
@@ -10,57 +10,72 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   Timer _timer;
 
-  @override
-  void initState() {
-    super.initState();
-    _timer = Timer(const Duration(seconds: 2), _onShowLogin);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _timer = Timer(const Duration(seconds: 2), _onShowLogin);
+  // }
 
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _timer.cancel();
+  //   super.dispose();
+  // }
 
-  Future<void> _onShowLogin() async {
-    bool firstLogin = true;
-    await AuthService().checkFirstLogin().then((result) {
-      firstLogin = result;
-    });
+  // Future<void> _onShowLogin() async {
+  //   bool firstLogin = true;
+  //   await AuthService().checkFirstLogin().then((result) {
+  //     firstLogin = result;
+  //   });
 
-    if (firstLogin) {
-      Navigator.of(context).pushReplacementNamed('/LoginScreen');
-    } else {
-      Navigator.of(context).pushReplacementNamed('/HomeScreen');
-    }
-  }
+  //   if (firstLogin) {
+  //     Navigator.of(context).pushReplacementNamed('/LoginScreen');
+  //   } else {
+  //     Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Container(
-        //decoration: ,
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 100.0,
+        decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                stops: [0.1, 0.8],
+                colors: [Color(0xCC272741), Color(0x00272741)],
+              ),
             ),
-            Image.asset("assets/images/escudo.png", width: 40, height: 40),
-            Flexible(
-              flex: 2,
-              child: SafeArea(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 64.0, horizontal: 16.0),
-                  alignment: Alignment.bottomCenter,
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.symmetric(vertical: 100)),
+              Container(
+                  child: Image.asset("assets/images/escudo.png",
+                      width: 200, height: 200)),
+              Flexible(
+                flex: 2,
+                child: SafeArea(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 100.0, horizontal: 16.0),
+                    alignment: Alignment.bottomCenter,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(child: Text('Created By Wisclic Tech'),)
-          ],
+              // Container(
+              //   padding: EdgeInsets.only(bottom: 20),
+              //   child: Text('Created By Wisclic Tech', style: TextStyle(
+              //     color: Colors.white,
+              //     fontSize: 18,
+              //     fontWeight: FontWeight.w600
+              //   ),))
+            ],
+          ),
         ),
       ),
     );
